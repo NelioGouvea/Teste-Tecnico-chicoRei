cb64d995c82bd0ba4df5c9ac0fd9b54b47c6d12f
Nelio <nelio.gouvea@codejr.com.br>
2022-02-17 22:01:06 -0300

    Interface Vue aplicada no laravel e components products criada

---
+++ resources/js/components/Products.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <div class="card " style="width: 18rem; ">
+        <img class="card-img-top" alt="Camiseta" :src="img">
+        <div class="card-body">
+            <h6 class="card-text">{{ title }}</h6>
+            <strong><p class="card-text">R$ {{ price.toFixed(2) }}</p></strong>
+            <p class="card-text">4x R$ {{ (price/4).toFixed(2) }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    props: {
+            title: String,
+            price: Number,
+            img: String,
+        },
+    name: "Products",
+    };
+</script>
